0a1a072281e309b343cfc8239b38cd3fe32df2b4
Isadora Moreira <moreirassisadora@gmail.com>
2022-03-18 16:01:52 -0300

    adicionado create, list, edit e delete (MySQL)

--- MySQL/Contexto.cs
+++ MySQL/Contexto.cs
@@ -1,10 +1,11 @@
-﻿using MySQL.Entidades;
+﻿using Microsoft.EntityFrameworkCore;
+using MySQL.Entidades;
 
 namespace MySQL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
-        public Contexto(DbContextoption<Contexto> option) : base(option) { }
+        public Contexto(DbContextOptions<Contexto> option) : base(option) { }
 
         public DbSet<Usuario> USUARIOS { get; set; }
     }
